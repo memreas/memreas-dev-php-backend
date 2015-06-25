a26bff1c8d1de1c02ffdae0e23ac7889a6cfebce
john meah <johnmeah@memreas.com>
2015-06-25 12:57:27 -0400

    .

--- bewgitpush.sh
+++ bewgitpush.sh
@@ -24,5 +24,5 @@ cp module/Application/src/Application/Model/MemreasConstants.localhost.php modul
 #
 # curl url to pull latest on backend
 #
-curl https://memreasdev-backend.memreas.com/?action=clearlog
-curl https://memreasdev-backend.memreas.com/?action=gitpull
+curl -d https://memreasdev-backend.memreas.com/?action=clearlog
+curl -d https://memreasdev-backend.memreas.com/?action=gitpull
