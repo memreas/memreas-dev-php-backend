1eca360e2fc16022dcacd505e86242be20c365ed
john meah <johnmeah@memreas.com>
2015-09-13 13:22:01 -0400

    .

--- update_ffmpeg_build.sh
+++ update_ffmpeg_build.sh
@@ -35,7 +35,7 @@ cd $source_dir
 git clone --depth 1 git://github.com/yasm/yasm.git
 cd yasm
 autoreconf -fiv
-./configure --prefix="${build_dir}" --bindir="${bin_dir}"
+./configure --prefix="$build_dir" --bindir="$bin_dir"
 make
 make install
 make distclean
@@ -44,10 +44,10 @@ make distclean
 ##############
 # Install x264
 ##############
-cd ${source_dir}
+cd $source_dir
 git clone --depth 1 git://git.videolan.org/x264
 cd x264
-./configure --prefix="${build_dir}" --bindir="${bin_dir}" --enable-static
+./configure --prefix="$build_dir" --bindir="$bin_dir" --enable-static
 make
 make install
 make distclean
@@ -57,8 +57,8 @@ make distclean
 ##############
 cd $source_dir
 hg clone https://bitbucket.org/multicoreware/x265
-cd ${source_dir}x265/build/linux
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="${build_dir}" -DENABLE_SHARED:bool=off ../../source
+cd $source_dirx265/build/linux
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$build_dir" -DENABLE_SHARED:bool=off ../../source
 make
 make install
 
@@ -69,7 +69,7 @@ cd $source_dir
 git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
 cd fdk-aac
 autoreconf -fiv
-./configure --prefix="${build_dir}" --disable-shared
+./configure --prefix="$build_dir" --disable-shared
 make
 make install
 make distclean
@@ -81,7 +81,7 @@ cd $source_dir
 curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 tar xzvf lame-3.99.5.tar.gz
 cd lame-3.99.5
-./configure --prefix="${build_dir}" --bindir="${bin_dir}" --disable-shared --enable-nasm
+./configure --prefix="$build_dir" --bindir="$bin_dir" --disable-shared --enable-nasm
 make
 make install
 make distclean
@@ -93,7 +93,7 @@ cd $source_dir
 git clone git://git.opus-codec.org/opus.git
 cd opus
 autoreconf -fiv
-./configure --prefix="${build_dir}" --disable-shared
+./configure --prefix="$build_dir" --disable-shared
 make
 make install
 make distclean
@@ -105,7 +105,7 @@ cd $source_dir
 curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
 tar xzvf libogg-1.3.2.tar.gz
 cd libogg-1.3.2
-./configure --prefix="${build_dir" --disable-shared
+./configure --prefix="$build_dir" --disable-shared
 make
 make install
 make distclean
@@ -117,7 +117,7 @@ cd $source_dir
 curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
 tar xzvf libvorbis-1.3.4.tar.gz
 cd libvorbis-1.3.4
-LDFLAGS="-L ${build_dir}lib" CPPFLAGS="-I ${build_dir}include" ./configure --prefix="${build_dir}" --with-ogg="${build_dir}" --disable-shared
+LDFLAGS="-L $build_dirlib" CPPFLAGS="-I $build_dirinclude" ./configure --prefix="$build_dir" --with-ogg="$build_dir" --disable-shared
 make
 make install
 make distclean
@@ -128,7 +128,7 @@ make distclean
 cd $source_dir
 git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
-PKG_CONFIG_PATH="${build_dir)lib/pkgconfig" ./configure --prefix="${build_dir)" --extra-cflags="-I ${build_dir)include" --extra-ldflags="-L ${build_dir)lib" --bindir="${bin_dir}" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265
+PKG_CONFIG_PATH="$build_dirlib/pkgconfig" ./configure --prefix="$build_dir" --extra-cflags="-I $build_dirinclude" --extra-ldflags="-L $build_dirlib" --bindir="$bin_dir" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265
 make
 make install
 make distclean
