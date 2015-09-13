568d72866616fb3dd0b5407a77f617d96cf60254
john meah <johnmeah@memreas.com>
2015-09-12 21:51:13 -0400

    .

--- update_ffmpeg_build.sh
+++ update_ffmpeg_build.sh
@@ -7,14 +7,14 @@
 #vars
 cmd="" 
 base_dir="/var/www/memreas_ffmpeg_install.bak/"
-source_dir="/var/www/memreas_ffmpeg_install/ffmpeg_sources/"
-build_dir="/var/www/memreas_ffmpeg_install/ffmpeg_build/"
-bin_dir="/var/www/memreas_ffmpeg_install/bin/"
+source_dir=$base_dir+"ffmpeg_sources/"
+build_dir=$base_dir+"ffmpeg_build/"
+bin_dir=$base_dir+"bin/"
 
 ####################################
 # remove old files and dependencies
 ####################################
-cmd="rm -rf $base_dir/ffmpeg_build $base_dir/bin/{ffmpeg,ffprobe,ffserver,lame,vsyasm,x264,x265,yasm,ytasm}"
+cmd="rm -rf $build_dir $bin_dir{ffmpeg,ffprobe,ffserver,lame,vsyasm,x264,x265,yasm,ytasm}"
 echo $cmd;
 cmd="sudo yum install autoconf automake cmake gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel"
 echo $cmd;
@@ -22,13 +22,13 @@ echo $cmd;
 ##############
 # Update Yasm
 ##############
-cmd="cd $base_dir/ffmpeg_sources/yasm"
+cmd="cd $source_dir"+"yasm"
 echo $cmd;
 cmd="make distclean"
 echo $cmd;
 cmd="git pull"
 echo $cmd;
-cmd="./configure --prefix=\"$base_dir/ffmpeg_build\" --bindir=\"$base_dir/bin\""
+cmd="./configure --prefix=\"$build_dir\" --bindir=\"$bin_dir\""
 echo $cmd;
 cmd="make"
 echo $cmd;
@@ -38,13 +38,13 @@ echo $cmd;
 ##############
 # Update x264
 ##############
-cmd="cd $base_dir/ffmpeg_sources/x264"
+cmd="cd $source_dir"+"x264"
 echo $cmd;
 cmd="make distclean"
 echo $cmd;
 cmd="git pull"
 echo $cmd;
-cmd="./configure --prefix=\"$base_dir/ffmpeg_build\" --bindir=\"$base_dir/bin\" --enable-static"
+cmd="./configure --prefix=\"$build_dir\" --bindir=\"$bin_dir\" --enable-static"
 echo $cmd;
 cmd="make"
 echo $cmd;
@@ -55,15 +55,15 @@ echo $cmd;
 ##############
 # Update x265
 ##############
-cmd="cd $base_dir/ffmpeg_sources/x265"
+cmd="cd $source_dir"+"x265"
 echo $cmd;
-cmd="rm -rf ~/ffmpeg_sources/x265/build/linux/*"
+cmd="rm -rf $source_dir"+"x265/build/linux/*"
 echo $cmd;
 cmd="hg update"
 echo $cmd;
-cmd="cd ~/ffmpeg_sources/x265/build/linux"
+cmd="cd $source_dir"+"x265/build/linux"
 echo $cmd;
-cmd="cmake -G \"Unix Makefiles\" -DCMAKE_INSTALL_PREFIX=\"$base_dir/ffmpeg_build\" -DENABLE_SHARED:bool=off ../../source"
+cmd="cmake -G \"Unix Makefiles\" -DCMAKE_INSTALL_PREFIX=\"$build_dir\" -DENABLE_SHARED:bool=off ../../source"
 echo $cmd;
 cmd="make"
 echo $cmd;
@@ -73,13 +73,13 @@ echo $cmd;
 #################
 # Update fdk_aac
 #################
-cmd="cd $base_dir/ffmpeg_sources/fdk_aac"
+cmd="cd $source_dir"+"fdk_aac"
 echo $cmd;
 cmd="make distclean"
 echo $cmd;
 cmd="git pull"
 echo $cmd;
-cmd="./configure --prefix=\"$base_dir/ffmpeg_build\" --disable-shared"
+cmd="./configure --prefix=\"$build_dir\" --disable-shared"
 echo $cmd;
 cmd="make"
 echo $cmd;
@@ -89,13 +89,13 @@ echo $cmd;
 #################
 # Update libvpx
 #################
-cmd="cd $base_dir/ffmpeg_sources/libvpx"
+cmd="cd $source_dir"+"libvpx"
 echo $cmd;
 cmd="make clean"
 echo $cmd;
 cmd="git pull"
 echo $cmd;
-cmd="./configure --prefix=\"$base_dir/ffmpeg_build\" --disable-examples"
+cmd="./configure --prefix=\"$build_dir\" --disable-examples"
 echo $cmd;
 cmd="make"
 echo $cmd;
@@ -105,13 +105,13 @@ echo $cmd;
 #################
 # Update ffmpeg
 #################
-cmd="cd $base_dir/ffmpeg_sources/ffmpeg"
+cmd="cd $source_dir"+"ffmpeg"
 echo $cmd;
 cmd="make distclean"
 echo $cmd;
 cmd="git pull"
 echo $cmd;
-cmd="PKG_CONFIG_PATH=\"$base_dir/ffmpeg_build/lib/pkgconfig\" ./configure --prefix=\"$base_dir/ffmpeg_build\" --extra-cflags=\"-I$base_dir/ffmpeg_build/include\" --extra-ldflags=\"-L $base_dir/ffmpeg_build/lib\" --bindir=\"$base_dir/bin\" --pkg-config-flags=\"--static\" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265"
+cmd="PKG_CONFIG_PATH=\"$build_dir"+"lib/pkgconfig\" ./configure --prefix=\"$build_dir\" --extra-cflags=\"-I $build_dir"+"include\" --extra-ldflags=\"-L $build_dir"+"lib\" --bindir=\"$bin_dir\" --pkg-config-flags=\"--static\" --enable-gpl --enable-nonfree --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265"
 echo $cmd;
 cmd="make"
 echo $cmd;
