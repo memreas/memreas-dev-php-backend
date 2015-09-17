5b57fd61b109845a60813f769a63d7a67ff23760
john meah <johnmeah@memreas.com>
2015-09-17 11:39:18 -0400

    update to constants for ffmpeg location

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -5,32 +5,60 @@
 // ///////////////////////////////
 namespace Application\Model;
 
-class MemreasConstants {
+class MemreasConstants
+{
+
     const AWS_APPKEY = 'AKIAJMXGGG4BNFS42LZA';
+
     const AWS_APPSEC = 'xQfYNvfT0Ar+Wm/Gc4m6aacPwdT5Ors9YHE/d38H';
+
     const AWS_APPREG = 'us-east-1';
-    const MEMREAS_TRANSCODER_FFMPEG = '/var/www/memreas_ffmpeg_install/bin/ffmpeg';
-	const MEMREAS_TRANSCODER_FFPROBE = '/var/www/memreas_ffmpeg_install/bin/ffprobe';
-	const MEMREAS_TRANSCODER_FFMPEG_LOCAL = '/usr/local/bin/ffmpeg';
-	const MEMREAS_TRANSCODER_FFPROBE_LOCAL = '/usr/local/bin/ffprobe';
-	const SIGNURLS = false;
-	const EXPIRES = 3600; // 1 hour should be enough to transcode
-	const MEMREASDB = 'memreasintdb';
-	const MEMREASBEDB = 'memreasbackenddb';
-	const S3BUCKET = "memreasdevsec";
-	const S3HLSBUCKET = "memreasdevhls";
-	const QUEUEURL = 'https://sqs.us-east-1.amazonaws.com/004184890641/memreasdev-bewq';
-	const URL = "/index";
-	const DATA_PATH = "/data/";
-	const MEDIA_PATH = "/media/";
-	const IMAGES_PATH = "/images/";
-	const THUMBNAILS_PATH = "/images/thumbnails/";
-	const USERIMAGE_PATH = "/media/userimage/";
-	const FOLDER_PATH = "/data/media/";
-	const FOLDER_AUDIO = "upload_audio";
-	const FOLDER_VIDEO = "uploadVideo";
-	const VIDEO = "/data/media/uploadVideo";
-	const AUDIO = "/data/media/upload_audio";
-	const CLOUDFRONT_STREAMING_HOST = 'http://s1iq2cbtodqqky.cloudfront.net/';
-	const CLOUDFRONT_DOWNLOAD_HOST = 'http://d1ckv7o9k6o3x9.cloudfront.net/';
+
+    const MEMREAS_TRANSCODER_FFMPEG = 'ffmpeg';
+
+    const MEMREAS_TRANSCODER_FFPROBE = 'ffprobe';
+
+    const MEMREAS_TRANSCODER_FFMPEG_LOCAL = 'ffmpeg';
+
+    const MEMREAS_TRANSCODER_FFPROBE_LOCAL = 'ffprobe';
+
+    const SIGNURLS = false;
+
+    const EXPIRES = 3600;
+    // 1 hour should be enough to transcode
+    const MEMREASDB = 'memreasintdb';
+
+    const MEMREASBEDB = 'memreasbackenddb';
+
+    const S3BUCKET = "memreasdevsec";
+
+    const S3HLSBUCKET = "memreasdevhls";
+
+    const QUEUEURL = 'https://sqs.us-east-1.amazonaws.com/004184890641/memreasdev-bewq';
+
+    const URL = "/index";
+
+    const DATA_PATH = "/data/";
+
+    const MEDIA_PATH = "/media/";
+
+    const IMAGES_PATH = "/images/";
+
+    const THUMBNAILS_PATH = "/images/thumbnails/";
+
+    const USERIMAGE_PATH = "/media/userimage/";
+
+    const FOLDER_PATH = "/data/media/";
+
+    const FOLDER_AUDIO = "upload_audio";
+
+    const FOLDER_VIDEO = "uploadVideo";
+
+    const VIDEO = "/data/media/uploadVideo";
+
+    const AUDIO = "/data/media/upload_audio";
+
+    const CLOUDFRONT_STREAMING_HOST = 'http://s1iq2cbtodqqky.cloudfront.net/';
+
+    const CLOUDFRONT_DOWNLOAD_HOST = 'http://d1ckv7o9k6o3x9.cloudfront.net/';
 }
