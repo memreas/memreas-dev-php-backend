377e95b21d098786f9a61b8490fb10d491f9b82b
john meah <johnmeah@memreas.com>
2015-09-20 18:01:17 -0400

    .

--- module/Application/src/Application/memreas/MemreasTranscoder.php
+++ module/Application/src/Application/memreas/MemreasTranscoder.php
@@ -632,8 +632,7 @@ class MemreasTranscoder
                 // Debugging - log table entry
                 Mlog::addone(
                         __CLASS__ . __METHOD__ . '::$this->persistMedia($this->memreas_media, 
-                        $memreas_media_data_array)', 
-                        $this->transcode_status);
+                        $memreas_media_data_array)', $this->transcode_status);
                 Mlog::addone(
                         __CLASS__ . __METHOD__ . __LINE__ .
                                  '::$this->memreas_media_metadata::after::', 
@@ -945,7 +944,7 @@ class MemreasTranscoder
                     // ' -x265-params crf=28 -c:a aac -strict -2 -vbr 4 ';
                     $qv = ' -c:v libx265 ' . '-preset ' .
                              $this->compression_preset_1080p .
-                             ' -pix_fmt yuv420p -x265-params crf=28 ' .
+                             ' -x265-params crf=28 ' . ' --profile main ' .
                              '-c:a aac -strict experimental  ' . '-b:a 128k ';
                     $transcoded_file = $this->homeDir . self::CONVDIR .
                              self::_1080PDIR . $this->MediaFileName . $mpeg4ext;
