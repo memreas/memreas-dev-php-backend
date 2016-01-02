46f6fa1ce34e6c2c2086fd34d2aefd1183a92f96
john meah <johnmeah@memreas.com>
2016-01-02 09:24:34 -0500

    . fix for timestampe in wake script

--- startup_worker.sh
+++ startup_worker.sh
@@ -2,6 +2,11 @@
 #Purpose = tail php_errors.log file
 #Created on 30-DEC-2015-2015
 
+# Define a timestamp function
+timestamp() {
+  date +"%T"
+}
+
 
 SERVICE=httpd
 WAIT=true
@@ -10,14 +15,14 @@ while [  $WAIT == true ]; do
 
 	if ps ax | grep -v grep | grep $SERVICE > /dev/null
 	then
-	    echo date +"%T $SERVICE service running, everything is fine\n" >> /var/www/memreas-dev-php-backend/wakeup.log
+	    echo timestamp + " $SERVICE service running, everything is fine\n" >> /var/www/memreas-dev-php-backend/wakeup.log
  	    echo curl https://memreasdev-backend.memreas.com/?action=wakeup >> /var/www/memreas-dev-php-backend/wakeup.log
  	    break; 
 	else
-	    echo date +"%T $SERVICE is not running will sleep until started..." >> /var/www/memreas-dev-php-backend/wakeup.log
+	    echo timestamp + " $SERVICE is not running will sleep until started..." >> /var/www/memreas-dev-php-backend/wakeup.log
 	    sleep 5
 	fi
 
 done
-echo date +"%T startup_worker.sh complete" >> /var/www/memreas-dev-php-backend/wakeup.log 
+echo timestamp + " startup_worker.sh complete" >> /var/www/memreas-dev-php-backend/wakeup.log 
 
