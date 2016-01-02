c62e78f57ff830b09e924aff5a6697af252c8f78
john meah <johnmeah@memreas.com>
2016-01-02 09:41:34 -0500

    .fixing logging for startup script

--- startup_worker.sh
+++ startup_worker.sh
@@ -11,21 +11,28 @@ timestamp() {
 SERVICE=httpd
 WAIT=true
 OUTPUTFILE=/var/www/memreas-dev-php-backend/wakeup.log
+CLEARURL=https://memreasdev-backend.memreas.com/?action=clearlog
+GITPULLURL=https://memreasdev-backend.memreas.com/?action=gitpull
+WAKEUPURL=https://memreasdev-backend.memreas.com/?action=wakeup
 
+rm $OUTPUTFILE
 while [  $WAIT == true ]; do
 
 	if ps ax | grep -v grep | grep $SERVICE > /dev/null
 	then
-	    echo timestamp + " $SERVICE service running, everything is fine\n" >> $OUTPUTFILE
-	    curl https://memreasdev-backend.memreas.com/?action=clearlog >> $OUTPUTFILE
-		curl https://memreasdev-backend.memreas.com/?action=gitpull	>> $OUTPUTFILE
- 	    curl https://memreasdev-backend.memreas.com/?action=wakeup >> $OUTPUTFILE
+	    echo "$(timestamp) $SERVICE service running, everything is fine\n" >> $OUTPUTFILE
+	    echo "calling $CLEARURL"
+	    curl $CLEARURL >> $OUTPUTFILE
+	    echo "calling $GITPULLURL"
+		curl $GITPULLURL	>> $OUTPUTFILE
+	    echo "calling $WAKEUPURL"
+		curl $WAKEUPURL	>> $OUTPUTFILE
  	    break; 
 	else
-	    echo timestamp + " $SERVICE is not running will sleep until started..." >> $OUTPUTFILE
+	    echo "$(timestamp) $SERVICE is not running will sleep until started..." >> $OUTPUTFILE
 	    sleep 5
 	fi
 
 done
-echo timestamp + " startup_worker.sh complete" >> $OUTPUTFILE 
+echo "$(timestamp) startup_worker.sh complete" >> $OUTPUTFILE 
 
