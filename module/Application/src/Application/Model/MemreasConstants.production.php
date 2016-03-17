2b9ce85b0f6d808a20e6a8789943b4e517c39dce
john meah <johnmeah@memreas.com>
2016-03-17 15:54:45 -0400

    .

---
+++ module/Application/src/Application/Model/MemreasConstants.production.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Copyright (C) 2015 memreas llc. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+namespace Application\Model;
+
+class MemreasConstants {
+	const AWS_APPKEY = 'AKIAIXDH3Q23AP4KPZRA';
+	const AWS_APPSEC = 'TM+p8s9hlJwnKECCEKSVVhEOBjVhTWDG4ijIhJdk';
+	const AWS_APPKEY = 'AKIAISDIQFVJMWFYXCIA';
+	const AWS_APPSEC = 'eM5HG4MbYhkW1Jz1RWIdMapo2s+DbB+KnkhzTt91';
+	const AWS_APPREG = 'us-east-1';
+	const MEMREAS_TRANSCODER_FFMPEG = '/var/www/memreas_ffmpeg_install/bin/ffmpeg';
+	const MEMREAS_TRANSCODER_FFPROBE = '/var/www/memreas_ffmpeg_install/bin/ffprobe';
+	const MEMREAS_TRANSCODER_FFMPEG_LOCAL = '/usr/local/bin/ffmpeg';
+	const MEMREAS_TRANSCODER_FFPROBE_LOCAL = '/usr/local/bin/ffprobe';
+	const SIGNURLS = false;
+	const EXPIRES = 3600;
+	const SIZE_5MB = 5000000;
+	const SIZE_10MB = 10000000;
+	const SIZE_100MB = 100000000;
+	// 1 hour should be enough to transcode
+	const MEMREASDB = 'memreasintdb';
+	const MEMREASBEDB = 'memreasbackenddb';
+	const S3BUCKET = "memreasprodsec";
+	const S3HLSBUCKET = "memreasprodhlssec";
+	const CLOUDFRONT_HLSSTREAMING_HOST = 'https://d1fhgtf97i7jlq.cloudfront.net/';
+
+	// Redis section
+	const REDIS_SERVER_ENDPOINT = "10.178.192.105";
+	const REDIS_SERVER_USE = true;
+	const REDIS_SERVER_SESSION_ONLY = true;
+	const REDIS_SERVER_PORT = "6379";
+	
+	// 12hour handle for process
+	const REDIS_CACHE_TTL = 43200;
+	const URL = "/index";
+	const DATA_PATH = "/data/";
+	const MEDIA_PATH = "/media/";
+	const IMAGES_PATH = "/images/";
+	const THUMBNAILS_PATH = "/images/thumbnails/";
+	const USERIMAGE_PATH = "/media/userimage/";
+	const FOLDER_PATH = "/data/media/";
+	const FOLDER_AUDIO = "upload_audio";
+	const FOLDER_VIDEO = "uploadVideo";
+	const VIDEO = "/data/media/uploadVideo";
+	const AUDIO = "/data/media/upload_audio";
+	public static function fetchAWS() {
+		$sharedConfig = [ 
+				'region' => self::AWS_APPREG,
+				'version' => 'latest',
+				'credentials' => [ 
+						'key' => self::AWS_APPKEY,
+						'secret' => self::AWS_APPSEC 
+				] 
+		];
+		
+		return new \Aws\Sdk ( $sharedConfig );
+	}
+}
---
+++ prodbewgitpush.sh
@@ -0,0 +1,36 @@
+#Purpose = ssh or sftp into ec2 instance
+#Created on 1-NOV-2014
+#Author = John Meah
+#Version 1.0
+
+echo -n "Enter the details of your deployment (i.e. 4-FEB-2014 Updating this script.) > "
+read comment
+echo "You entered $comment"
+#set -v verbose #echo on
+
+#copy fe settings to push to git...
+#cp ./module/Application/src/Application/Model/MemreasConstants.bewQ.php ./module/Application/src/Application/Model/MemreasConstants.php
+
+#Update composer.phar and files
+#echo "Updating composer and vendor..."
+#php composer.phar self-update
+#php composer.phar update
+
+#zero out php_errors.log
+rm php_errors.log
+
+#Push to AWS
+echo "Committing to git..."
+git add .
+git commit -m "$comment"
+echo "Pushing to github..."
+set -v verbose #echo on
+git push 
+
+#cp module/Application/src/Application/Model/MemreasConstants.localhost.php module/Application/src/Application/Model/MemreasConstants.php
+
+#
+# curl url to pull latest on backend
+#
+curl https://memreasprod-backend.memreas.com/?action=clearlog
+curl https://memreasprod-backend.memreas.com/?action=gitpull
