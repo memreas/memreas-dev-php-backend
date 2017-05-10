760c5ca2dcd8a563bfa9090e3c3fee70a57ea2e1
john meah <johnmeah@memreas.com>
2017-05-10 16:57:02 -0400

    updated redis ip

--- module/Application/src/Application/Model/MemreasConstants.php
+++ module/Application/src/Application/Model/MemreasConstants.php
@@ -29,8 +29,7 @@ class MemreasConstants {
 	const CLOUDFRONT_HLSSTREAMING_HOST = 'https://d1fhgtf97i7jlq.cloudfront.net/';
 
 	// Redis section
-	//const REDIS_SERVER_ENDPOINT = "10.154.58.83";
-	const REDIS_SERVER_ENDPOINT = "172.31.11.12";
+	const REDIS_SERVER_ENDPOINT = "10.0.0.88";
 	const REDIS_SERVER_USE = true;
 	const REDIS_SERVER_SESSION_ONLY = true;
 	const REDIS_SERVER_PORT = "6379";
